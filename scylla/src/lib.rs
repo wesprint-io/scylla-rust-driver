365bf24c4fb2d0c1a634ba7cd28ec04bc29853d4
Axel Berardino <axel.berardino@gmail.com>
2025-12-18 17:27:27 +0100

    expose internal testing

--- scylla/src/lib.rs
+++ scylla/src/lib.rs
@@ -262,7 +262,6 @@ pub(crate) mod utils;
 #[cfg(test)]
 pub(crate) use utils::test_utils;
 
-#[cfg(all(scylla_unstable, feature = "unstable-testing"))]
 #[doc(hidden)]
 pub mod internal_testing {
     use scylla_cql::serialize::row::SerializedValues;
